4355d482659ae78b5957f9871246811838357739
huncijr <hunor.burian08@gmail.com>
2025-12-28 12:01:38 +0200

    another try

--- Frontend/src/Pages/HomePage.jsx
+++ Frontend/src/Pages/HomePage.jsx
@@ -27,7 +27,6 @@ const HomePage = () => {
   const { selecteddoctor, setSelectedDoctor } = useDoctor();
   const [doctors, setDoctors] = useState(null);
   const navigate = useNavigate();
-  const IMAGE_BASE_URL = import.meta.VITE_IMAGE_BASE_URL || "AA";
 
   useEffect(() => {
     console.log(import.meta.env.VITE_API_URL);
@@ -170,7 +169,7 @@ const HomePage = () => {
                   <div className="flex-1 flex flex-col p-1 ">
                     <figure className="h-80 overflow-hidden rounded-md w-full object-cover border-b-2 py-2 mb-2 border-secondary">
                       <img
-                        src={`${IMAGE_BASE_URL}${doctor.img}`}
+                        src={`${doctor.img}`}
                         alt="No image "
                         className="text-white w-full h-full object-cover  min-w-[100%] min-h-[100%]"
                       />
